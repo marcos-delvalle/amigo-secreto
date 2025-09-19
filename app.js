463bae49bf04ddf1cedd5bc6d19e473a0429fb62
Marcos del Valle <delvallemarcos05@gmail.com>
2025-09-19 18:21:35 -0300

    fix + mostrarNombres

--- app.js
+++ app.js
@@ -3,6 +3,24 @@ let amigos = [];
 
 function agregarAmigo(){
     let nombre = document.getElementById('amigo').value;
-    amigos.push(nombre)
-    document.getElementById('amigo').value = "";
+    if (nombre == ''){
+        alert("Por favor ingrese un nombre válido")
+        return
+    } else{
+        amigos.push(nombre)
+        document.getElementById('amigo').value = "";
+        mostrarNombres()
+    }
 }
+
+function mostrarNombres(){
+    //console.log("mostrando desde la funcion", amigos)
+    let lista = document.getElementById('listaAmigos')
+    lista.innerHTML = "";
+    amigos.forEach(nombreAmigo => {
+        let elementoLista = document.createElement('li');
+        elementoLista.textContent = nombreAmigo;
+        lista.appendChild(elementoLista);
+    });
+}
+
